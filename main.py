7a368d4cbee70bdd471d6cb4ae3cb6d7e7d493ad
n0s1kh1ya <gauravkumaryadav618@gmail.com>
2020-07-14 18:28:57 +0530

    added input

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from urllib.request import urlopen
 video_num = 1
 file_num = 1
+inp_link = input()
 while True:
     
-    link = 'https://videos-a.jwpsrv.com/content/conversions/Ux8FajpR/videos/ti073qxr-32240523.mp4-{}.ts'.format(video_num)
+    append_str = '{}.ts'.format(video_num)
+    link = inp_link + append_str
     response_url = urlopen(link)
     if response_url.getcode() == 200:
         file_name = 'Lecture_{}.mp4'.format(file_num) 
-        print(file_name)
         with open(file_name,'wb') as f:
             f.write(response_url.read())
         video_num += 1
         file_num += 1
-        print(file_num)
     else:
         print('Exited')
         break
