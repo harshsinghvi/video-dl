42253da7e76805954d8255a09b2ca74e4b076fc4
n0s1kh1ya <gauravkumaryadav618@gmail.com>
2020-07-14 18:07:32 +0530

    updated video

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import requests 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response)
+from urllib.request import urlopen
+video_num = 1
+file_num = 1
+while True:
+    
+    link = 'https://videos-a.jwpsrv.com/content/conversions/Ux8FajpR/videos/ti073qxr-32240523.mp4-{}.ts'.format(video_num)
+    response_url = urlopen(link)
+    if response_url.getcode() == 200:
+        file_name = 'Lecture_{}.mp4'.format(file_num) 
+        print(file_name)
+        with open(file_name,'wb') as f:
+            f.write(response_url.read())
+        video_num += 1
+        file_num += 1
+        print(file_num)
+    else:
+        print('Exited')
+        break
